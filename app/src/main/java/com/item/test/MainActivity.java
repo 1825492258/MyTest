da66964dea32feee97df6f557189968ad4410353
Administrator <1825492258@qq.com>
2017-11-12 19:34:55 +0800

    第一次提交

---
+++ app/src/main/java/com/item/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.item.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 用来写安卓艺术探究的demo
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
